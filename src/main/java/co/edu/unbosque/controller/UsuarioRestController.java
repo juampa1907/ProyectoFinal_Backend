d95a120209f77813d7e4610f3e275b3da3159f8c
Juampa1907 <juampasemi19@gmail.com>
2026-05-13 00:09:10 -0500

    Finalización Clase UsuarioRestController iniciando implementación de excepciones

--- src/main/java/co/edu/unbosque/controller/UsuarioRestController.java
+++ src/main/java/co/edu/unbosque/controller/UsuarioRestController.java
@@ -2,12 +2,12 @@ package co.edu.unbosque.controller;
 
 import co.edu.unbosque.entity.Usuario;
 import co.edu.unbosque.service.api.UsuarioServiceAPI;
-import co.edu.unbosque.utils.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -19,33 +19,39 @@ public class UsuarioRestController {
     private UsuarioServiceAPI usuarioServiceAPI;
     
     @GetMapping(value = "/getAll")
-    public List<Usuario> getAll() {
-        return usuarioServiceAPI.getAll();
+    public ResponseEntity<List<Usuario>> getAllUsuarios() {
+        return ResponseEntity.ok(usuarioServiceAPI.getAll());
     }
     
     @PostMapping(value = "/saveUsuario")
-    public ResponseEntity<Usuario> save(@RequestBody Usuario usuario) {
-        Usuario obj = usuarioServiceAPI.save(usuario);
-        return new ResponseEntity<>(obj, HttpStatus.OK);
+    public ResponseEntity<Usuario> saveUsuario(@RequestBody Usuario usuario) {
+        return  ResponseEntity.status(HttpStatus.CREATED).body(usuarioServiceAPI.save(usuario));
     }
     
     @GetMapping(value = "/findRecord/{id}")
-    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Integer id) throws ResourceNotFoundException {
-        Usuario usuario = usuarioServiceAPI.get(id);
-        if (usuario == null) {
-            throw new ResourceNotFoundException("Record not found for Usuario: " + id);
-        }
-        return ResponseEntity.ok().body(usuario);
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Integer id) {
+        return ResponseEntity.ok(usuarioServiceAPI.get(id));
     }
     
     @DeleteMapping(value = "/deleteUsuario/{id}")
-    public ResponseEntity<Usuario> delete(@PathVariable Integer id) {
-        Usuario usuario = usuarioServiceAPI.get(id);
-        if (usuario != null) {
-            usuarioServiceAPI.delete(id);
-            return new ResponseEntity<>(usuario, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<Void> deleteUsuario(@PathVariable Integer id) {
+       usuarioServiceAPI.delete(id);
+        return ResponseEntity.noContent().build();
     }
+
+    @PutMapping("/updateUsuario")
+    public ResponseEntity<Usuario> updateUsuario(@RequestBody Usuario usuario){
+        Usuario user = usuarioServiceAPI.get(usuario.getIdUsuario());
+        String passwordActual = user.getPassword();
+        user.setUsername(usuario.getUsername());
+        user.setPassword(usuario.getPassword());
+        user.setNombreApellido(usuario.getNombreApellido());
+        user.setIdRol(usuario.getIdRol());
+        user.setEstado(usuario.getEstado());
+        if (!usuario.getPassword().equals(passwordActual)){
+            user.setFechaUltClave(LocalDateTime.now());
+        }
+        return ResponseEntity.ok(usuarioServiceAPI.update(user));
+    }
+
 }
